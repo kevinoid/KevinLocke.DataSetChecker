7859a8413e23d8b8fec098d1af1ec38fe6388583
Kevin Locke <kevin@kevinlocke.name>
2018-01-04 14:34:58 -0700

    Create useful DataSetCheckerEventArgs.ToString
    So that events can be stringified in a useful form for users.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- DataSetCheckerEventArgs.cs
+++ DataSetCheckerEventArgs.cs
@@ -7,11 +7,14 @@
 namespace KevinLocke.DataSetChecker
 {
     using System;
+    using System.Text;
     using System.Xml;
     using System.Xml.Schema;
 
     public class DataSetCheckerEventArgs : EventArgs
     {
+        private static readonly DataSetXPathFinder XPathFinder = new DataSetXPathFinder();
+
         private readonly Exception exception;
         private readonly string message;
         private readonly XmlNode node;
@@ -32,5 +35,27 @@ namespace KevinLocke.DataSetChecker
         public XmlNode Node => this.node;
 
         public XmlSeverityType Severity => this.severity;
+
+        public override string ToString()
+        {
+            StringBuilder errMsg = new StringBuilder();
+            errMsg.Append(this.Severity).Append(": ").Append(this.Message);
+
+            if (this.Node != null)
+            {
+                errMsg.Append(" (at ")
+                    .Append(XPathFinder.FindXPathFromTablesOrRoot(this.Node))
+                    .Append(" in ")
+                    .Append(this.Node.BaseURI)
+                    .Append(')');
+            }
+
+            if (this.Exception != null)
+            {
+                errMsg.Append('\n').Append(this.Exception);
+            }
+
+            return errMsg.ToString();
+        }
     }
 }
