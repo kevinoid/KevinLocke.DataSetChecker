443f38642349cdc2bec60f6d21093087ca313f2d
Kevin Locke <kevin@kevinlocke.name>
2018-03-20 20:08:13 -0600

    Remove UTF-8 BOM from files
    BOM support/handling is inconsistent and introduces unnecessary
    complexity.  Since the byte order is irrelevant and all files are known
    to be UTF-8, it's unnecessary and already configured against by
    .editorconfig.  Remove it from existing files.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="AssemblyInfo.cs" company="Kevin Locke">
+// <copyright file="AssemblyInfo.cs" company="Kevin Locke">
 // Copyright 2017 Kevin Locke &lt;kevin@kevinlocke.name&gt;
 // This file is part of KevinLocke.DataSetChecker,
 // publicly available under the MIT License.  See LICENSE.txt for details.
